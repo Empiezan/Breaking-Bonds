a918701df2e54a8ecfc214ac16f252469aa0e866
Empiezan <michaelchang@wustl.edu>
2018-03-12 16:19:22 -0500

    initial setup routing and reading of sorted teams from spreadsheet

---
+++ src/components/Scoreboard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../Scoreboard.css';
+import ReactGoogleSheetConnector from "react-google-sheet-connector"
+import { connectToSpreadsheet } from "react-google-sheet-connector";
+import { GoogleTable } from "react-google-sheet-connector"
+
+const API_KEY = 'AIzaSyCAwxMAHCezEbVndZl3-JPe-vsKn3jM0pA';
+const CLIENT_ID = '266547601449-a86le41arqjfaeu8r3qbvicbmb1npcco.apps.googleusercontent.com';
+const SPREADSHEET_ID = '1DTjxWQt0YgTUfjDNUPQGRWNxT9f8Q6R6hWv0_YOmOQM';
+
+class Scoreboard extends React.Component {
+    render() {
+        return (
+            <div className="Scoreboard">
+                <ReactGoogleSheetConnector clientid={CLIENT_ID}
+                                           apiKey={API_KEY}
+                                           spreadsheetId={SPREADSHEET_ID}
+                                           spinner={ <div className="loading-spinner"/> }>
+                    <GoogleTable sheetName="Sorted Teams" />
+                </ReactGoogleSheetConnector>
+            </div>
+        )
+    }
+}
+
+export default connectToSpreadsheet(Scoreboard);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import Clock from './components/Clock';
 import registerServiceWorker from './registerServiceWorker';
+import Scoreboard from "./components/Scoreboard";
+import { BrowserRouter, Route } from 'react-router-dom';
+// import Scoreboard from "./Scoreboard";
+// import ScoreBoardItem from './components/score_board_item';
+// import { ScoreBoardList } from './components/score_board_list';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+
+// const App = () => {
+//     return <div>
+//         <ScoreBoardItem/>
+//     </div>
+// }
+
+
+// let View = (
+//     <BrowserRouter>
+//         <Route path="/" component={App} />
+//         <Route path="/scoreboard" component={Scoreboard} />
+//     </BrowserRouter>
+// );
+
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={App} />
+            <Route path="/scoreboard" component={Scoreboard} />
+        </div>
+    </BrowserRouter>, document.getElementById('root')
+);
+
+// ReactDOM.render(View, document.getElementById('root'));
+// ReactDOM.render(<Clock/>, document.getElementById('clock'));
+// ReactDOM.render(<Scoreboard/>, document.getElementById('board'));
+// ReactDOM.render(<Scoreboard />, document.getElementById('root'));
 registerServiceWorker();
