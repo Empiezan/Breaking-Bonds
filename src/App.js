15a9034efe98831d9be9d794ac66919ff4990cf3
Empiezan <michaelchang@wustl.edu>
2018-03-17 20:40:30 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import './App.css';
+import GraderContainer from './containers/GraderContainer';
+import Scoreboard from "./components/Scoreboard";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Button } from 'react-bootstrap';
 
-class App extends Component {
+const App = () => (
+  <Router>
+    <div>
 
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
-}
+      <Link to="/">
+      <Button>
+      Grading
+      </Button>
+      </Link>
+
+      <Link to="/scoreboard">
+      <Button>
+      Scoreboard
+      </Button>
+      </Link>
+
+
+
+
+      <hr />
+      <Route exact path="/" component={GraderContainer} />
+      <Route path="/scoreboard" component={Scoreboard} />
+    </div>
+  </Router>
+);
 
 export default App;
--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -8,10 +8,37 @@ const API_KEY = 'AIzaSyCAwxMAHCezEbVndZl3-JPe-vsKn3jM0pA';
 const CLIENT_ID = '266547601449-a86le41arqjfaeu8r3qbvicbmb1npcco.apps.googleusercontent.com';
 const SPREADSHEET_ID = '1DTjxWQt0YgTUfjDNUPQGRWNxT9f8Q6R6hWv0_YOmOQM';
 
-const Scoreboard = (props) => {
-    // render() {
+class Scoreboard extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          time: new Date().toLocaleString().split(",")[1]
+      };
+
+  }
+
+  componentDidMount() {
+      this.intervalID = setInterval(
+          () => this.tick(),
+          1000
+      );
+  }
+
+  componentWillUnmount() {
+      clearInterval(this.intervalID);
+  }
+
+  tick() {
+      this.setState({
+          time: new Date().toLocaleString().split(",")[1],
+      });
+  }
+    render() {
         return (
             <div className="Scoreboard">
+                <p className="App-clock">
+                    {this.state.time}
+                </p>
                 <ReactGoogleSheetConnector clientid={CLIENT_ID}
                                            apiKey={API_KEY}
                                            spreadsheetId={SPREADSHEET_ID}
@@ -21,7 +48,7 @@ const Scoreboard = (props) => {
             </div>
 
         )
-    // }
+    }
 }
 
 export default connectToSpreadsheet(Scoreboard);
